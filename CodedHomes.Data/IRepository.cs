d13e4b2a13efbf5039a09ce5129dd0a4709be89c
Servando Coria <servando.coria@freewayinsurance.com>
2016-01-15 08:18:54 -0800

    adding a generic and concrete repositories

---
+++ CodedHomes.Data/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodedHomes.Data
+{
+    /// <summary>
+    /// interface for all the repos, only apply to classes
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T: class
+    {
+        IQueryable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(int id);
+        void Detach(T entity);
+    }
+}
---
+++ CodedHomes.Data/UsersRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CodedHomes.Models;
+using System.Data.Entity;
+
+namespace CodedHomes.Data
+{
+    public class UsersRepository : GenericRepository<User>
+    {
+        public UsersRepository(DbContext context):base(context)
+        {
+        }
+    }
+}
