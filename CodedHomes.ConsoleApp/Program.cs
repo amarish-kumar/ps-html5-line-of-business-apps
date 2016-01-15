433da6910f3c64a4434d6254a3eb8419a2386e6d
Servando Coria <servando.coria@freewayinsurance.com>
2016-01-15 08:04:02 -0800

    initializing the database with a console application

---
+++ CodedHomes.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.Entity;
+using CodedHomes.Data;
+
+namespace CodedHomes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Initializing Database...");
+                DataContext context = new DataContext();
+                context.Database.Initialize(true);
+
+                Console.WriteLine("Done...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
