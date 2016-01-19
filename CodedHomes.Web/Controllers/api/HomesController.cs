3bbbbdb001c759bcf21f6c2172bb32078d1b70a2
Servando Coria <servando.coria@freewayinsurance.com>
2016-01-18 17:15:52 -0800

    implementing Homes API: Put Method

--- CodedHomes.Web/Controllers/api/HomesController.cs
+++ CodedHomes.Web/Controllers/api/HomesController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 
 using CodedHomes.Data;
 using CodedHomes.Models;
+using System.Data.Entity.Infrastructure;
 
 namespace CodedHomes.Web.Controllers
 {
@@ -37,12 +38,45 @@ namespace CodedHomes.Web.Controllers
             return home;
         }
 
-        /*
         [System.Web.Http.Authorize(Roles = "admin, manager, user")]
         public HttpResponseMessage Put(int id, Home home)
-        { 
+        {
+            // some validation error
+            if (!ModelState.IsValid)
+            {
+                return Request.
+                    CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
 
+            if (id != home.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            Home existingHome = this._unit.Homes.GetById(id);
+            _unit.Homes.Detach(existingHome);
+
+            // keep original CreatedOn value
+            home.CreatedOn = existingHome.CreatedOn;
+
+            this._unit.Homes.Update(home);
+
+            try
+            {
+                this._unit.SaveChanges();
+
+                // return an expicit value to avoid the fail callback
+                //beign incorrectly invoiked
+                return Request.CreateResponse(HttpStatusCode.OK, "{success:'true', verb:'PUT'}");
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
         }
-        */
     }
 }
