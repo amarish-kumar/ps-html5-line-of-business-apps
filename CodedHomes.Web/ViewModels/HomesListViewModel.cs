56e45a45ad645e8a9c7ae95392ed75b3de98d746
Servando Coria <servando.coria@freewayinsurance.com>
2016-01-20 08:19:05 -0800

    implementing the HomesListViewModel class: does not compile because of ImagesUrlPrefix

---
+++ CodedHomes.Web/ViewModels/HomesListViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using CodedHomes.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CodedHomes.Web.ViewModels
+{
+    public class HomesListViewModel : ViewModelBase
+    {
+        public IList<Home> Homes { get; set; }
+        public string HomesJSON
+        {
+            get {
+                // lowercase properties
+                JsonSerializerSettings settings = new JsonSerializerSettings();
+
+                settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+                var homes = JsonConvert.SerializeObject(this.Homes, settings);
+                return homes;
+            }
+        }
+
+        public string ImageUrlPrefix
+        {
+            get { return CodedHomes.Web.Config.ImagesUrlPrefix; }
+        }
+    }
+}
