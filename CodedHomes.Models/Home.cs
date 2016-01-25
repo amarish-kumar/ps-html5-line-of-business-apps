106a613235c51cd3e795d5cae2e89d7fbb72d484
Servando Coria <servando.coria@freewayinsurance.com>
2016-01-25 08:27:56 -0800

    updating the application configuration

--- CodedHomes.Models/Home.cs
+++ CodedHomes.Models/Home.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace CodedHomes.Models
 {
     public class Home : IAuditInfo
     {
         public int Id { get; set; }
+        [Required]
         public string StreetAddress { get; set; }
         public string StreetAddress2 { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public int ZipCode { get; set; }
+        [Required]
         public int? Bathrooms { get; set; }
+        [Required]
         public int? Bedrooms { get; set; }
+        [Required]
         public int? SquareFeet { get; set; }
+        [Required]
         public int? Price { get; set; }
+        [Required]
         public string Description { get; set; }
         public string ImageName { get; set; }
         public DateTime CreatedOn { get; set; }
--- CodedHomes.Web/App_Start/CustomGlobalConfig.cs
+++ CodedHomes.Web/App_Start/CustomGlobalConfig.cs
@@ -5,6 +5,8 @@ using System.Web;
 
 using CodedHomes.Web.Filters;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Validation.Providers;
 
 namespace CodedHomes.Web
 {
@@ -12,6 +14,20 @@ namespace CodedHomes.Web
     {
         public static void Customize(HttpConfiguration config)
         {
+            // fixes issues with overly-aggressive validation provider:
+            //
+            config.Services.RemoveAll(
+                typeof(System.Web.Http.Validation.ModelValidatorProvider),
+                v => v is InvalidModelValidatorProvider
+            );
+
+            // approach via @encosia
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // approach via @john_papa. ensure any json message follow this rule
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             config.Filters.Add(new ValidationActionFilter());
         }
     }
