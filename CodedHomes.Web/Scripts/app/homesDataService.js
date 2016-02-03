2436994efc6b28ba7f96092dfa8610072ff8c8b7
Servando Coria <servando.coria@freewayinsurance.com>
2016-02-02 16:37:19 -0800

    updating the client data service for local persistence

--- CodedHomes.Web/Scripts/app/homesDataService.js
+++ CodedHomes.Web/Scripts/app/homesDataService.js
@@ -46,7 +46,81 @@ var homesDataService = (function () {
                 contentType: false,
                 data: data
             });
-        }
+        },
+        localKey: 'codedhomes-new-homes',
+        saveLocal: function(newHome) {
+                if (Modernizr.localstorage) {
+                    var
+                        ls = window.localStorage,
+                        key = ds.localKey;
+
+                    if (ls.getItem(key) == null) {
+                        ls.setItem(key, '[]');
+                    }
+
+                    var existing = ls.getItem(key);
+                    existing = ds.parse(existing);
+
+                    existing.push(newHome)
+
+                    var serialized = JSON.stringify(existing);
+                    ls.setItem(key, serialized);
+                }
+        },
+        // reviver syntax: http://www.json.org/js.html
+        parse: function(data) {
+            var parseWithReviver = function(d) {
+                return JSON.parse(d, function(key, value) {
+                    var type;
+                    if (value && typeof value === 'object') {
+                        type = value.type;
+                        if (typeof type === 'string' &&
+                            typeof window[type] === 'function') {
+                            return new (window[type])(value);
+                        }
+                    }
+                    return value;
+                });
+            };
+
+            var result = parseWithReviver(data);
+
+            if (_.isArray(result)) {
+                _.each(result, function(v, i) {
+                    if (typeof v === 'string') {
+                        result[i] = parseWithReviver(v);
+                    }
+                });
+            }
+
+            return result;
+        },
+        delLocal: function(home) {
+            var
+                key = homesDataService.localKey,
+                data = window.localStorage.getItem(key),
+                homes = {},
+                remaining = [];
+
+            debugger;
+
+            homes = ds.parse(data);
+
+            // difference from array
+            _.each(homes, function(v, i) {
+                if (!_.isEqual(homes[i], home)) {
+                    remaining.push(homes[i]);
+                }
+            });
+
+            if (remaining.length > 0) {
+                remaining = JSON.stringify(remaining);
+            } else {
+                remaining = '[]';
+            }
+
+            window.localStorage.setItem(key, remaining);
+        },
     };
 
     // run under the context of the ds context
@@ -55,8 +129,12 @@ var homesDataService = (function () {
     // make it public and return the promise
     return {
         save: ds.save,
+        saveLocal: ds.saveLocal,
         del: ds.del,
-        saveImage: ds.saveImage
+        delLocal: ds.delLocal,
+        saveImage: ds.saveImage,
+        parse: ds.parse,
+        localKey: ds.localKey
     };
 
 })();
